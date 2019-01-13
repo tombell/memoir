d3a692d7f6668da56c352cd0fcfd1c187d7643c2
Tom Bell <tomb@tomb.io>
2019-01-13 18:32:59 +0000

    Implement Open for database package

--- database/database.go
+++ database/database.go
@@ -1,6 +1,31 @@
 package database
 
-// Open ...
-func Open() error {
+import "database/sql"
+
+// Database ...
+type Database struct {
+	conn *sql.DB
+}
+
+// Close ...
+func (d *Database) Close() error {
+	if d.conn != nil {
+		return d.conn.Close()
+	}
+
 	return nil
+}
+
+// Open ...
+func Open(dsn string) (*Database, error) {
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &Database{conn: conn}, nil
 }
