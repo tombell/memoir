cb4a258c1116ae100835df4585816ec1c73cc100
Tom Bell <tomb@tomb.io>
2019-01-14 00:56:18 +0000

    Add basic flag/main structure to memoir and memoir-import

--- cmd/memoir-import/main.go
+++ cmd/memoir-import/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const helpText = `usage: memoir-import [args]
+`
+
+var (
+	version = flag.Bool("version", false, "")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+	os.Exit(2)
+}
+
 func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "memoir-import %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
 }
--- cmd/memoir/main.go
+++ cmd/memoir/main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const helpText = `usage: memoir [args]
+`
+
+var (
+	version = flag.Bool("version", false, "")
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, helpText)
+	os.Exit(2)
+}
+
 func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	if *version {
+		fmt.Fprintf(os.Stdout, "memoir %s (%s)\n", Version, Commit)
+		os.Exit(0)
+	}
 }
