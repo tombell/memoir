2dc5fba3394cfa855a14acf26fe14cb4eccc990f
Tom Bell <tomb@tomb.io>
2019-01-15 01:59:31 +0000

    Add indexes for columns we might search by

--- migrations/20190113235000_create_tracks_table.sql
+++ migrations/20190113235000_create_tracks_table.sql
@@ -9,6 +9,14 @@ CREATE TABLE tracks (
   key    VARCHAR (8) NOT NULL
 );
 
+CREATE INDEX tracks_artist_idx ON tracks ((lower(artist)));
+CREATE INDEX tracks_name_idx ON tracks ((lower(name)));
+CREATE INDEX tracks_genre_idx ON tracks ((lower(genre)));
+
 -- DOWN
+
+DROP INDEX IF EXISTS tracks_genre_idx CASCADE;
+DROP INDEX IF EXISTS tracks_name_idx CASCADE;
+DROP INDEX IF EXISTS tracks_artist_idx CASCADE;
 
 DROP TABLE IF EXISTS tracks CASCADE;
