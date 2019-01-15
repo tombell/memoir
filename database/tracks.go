f4097c5ad144dba6af62eec4d349196c86eecb86
Tom Bell <tomb@tomb.io>
2019-01-15 02:20:40 +0000

    Add InsertTrack and GetTrack functions

--- database/tracks.go
+++ database/tracks.go
@@ -1,6 +1,8 @@
 package database
 
-// TrackRecord ...
+import "database/sql"
+
+// TrackRecord  represents a single track row in the database.
 type TrackRecord struct {
 	ID     string
 	Artist string
@@ -8,4 +10,40 @@ type TrackRecord struct {
 	Genre  string
 	BPM    int
 	Key    string
+}
+
+// InsertTrack inserts a new track into the database.
+func (db *Database) InsertTrack(tx *sql.Tx, track *TrackRecord) error {
+	_, err := tx.Exec(sqlInsertTrack,
+		track.ID,
+		track.Artist,
+		track.Name,
+		track.Genre,
+		track.BPM,
+		track.Key)
+
+	return err
+}
+
+// GetTrack returns a single track with the given ID from the database.
+// Returns nil if the track doesn't exist.
+func (db *Database) GetTrack(id string) (*TrackRecord, error) {
+	var track TrackRecord
+
+	err := db.conn.QueryRow(sqlGetTrackByID, id).Scan(
+		&track.ID,
+		&track.Artist,
+		&track.Name,
+		&track.Genre,
+		&track.BPM,
+		&track.Key)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, nil
+	case err != nil:
+		return nil, err
+	default:
+		return &track, nil
+	}
 }
