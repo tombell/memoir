07526f8eb1ccc6f4fa52d900896485a7a2a327cb
Tom Bell <tomb@tomb.io>
2019-01-15 02:11:40 +0000

    Add initial queries for tracks

---
+++ database/queries_tracks.go
@@ -0,0 +1,25 @@
+package database
+
+const (
+	sqlInsertTrack = `
+		INSERT INTO tracks (
+			id,
+			artist,
+			name,
+			genre,
+			bpm,
+			key
+		) VALUES ($1, $2, $3, $4, $5, $6);`
+
+	sqlGetTrackByID = `
+		SELECT
+			id,
+			artist,
+			name,
+			genre,
+			bpm,
+			key
+		FROM tracks
+		WHERE id = $1
+		LIMIT 1`
+)
