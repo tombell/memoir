1df2869fa5928285c94f89ffc0c902bd87b4cb83
Tom Bell <tomb@tomb.io>
2019-01-17 01:18:45 +0000

    Move the function for read/parse CSV into parser package

--- cmd/memoir-import/main.go
+++ cmd/memoir-import/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"bytes"
-	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"os"
 	"time"
+
+	"github.com/tombell/memoir/parser"
 )
 
 const helpText = `usage: memoir-import [args]
@@ -52,36 +50,11 @@ func main() {
 		flag.Usage()
 	}
 
-	records, err := readSeratoExport(args[0])
+	records, err := parser.ParseSeratoExport(args[0])
 	exitIfError(err)
 
 	t, err := time.Parse(dateTimeFormat, records[0][0])
 	exitIfError(err)
 
 	fmt.Fprintf(os.Stdout, "tracklist from %v\n\n", t.Format(dateTimeFormat))
-}
-
-func readSeratoExport(filepath string) ([][]string, error) {
-	in, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		return nil, err
-	}
-
-	r := csv.NewReader(bytes.NewReader(in))
-
-	var records [][]string
-
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		records = append(records, record)
-	}
-
-	return records[1:], nil
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,35 @@
 package parser
+
+import (
+	"bytes"
+	"encoding/csv"
+	"io"
+	"io/ioutil"
+)
+
+// ParseSeratoExport reads and parses the exported CSV file from Serato, and
+// returns the parsed records.
+func ParseSeratoExport(filepath string) ([][]string, error) {
+	in, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	r := csv.NewReader(bytes.NewReader(in))
+
+	var records [][]string
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		records = append(records, record)
+	}
+
+	return records[1:], nil
+}
