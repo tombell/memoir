d6d4a8473c08363e0b96fcb86e85a792a4adaf13
Tom Bell <tomb@tomb.io>
2019-01-20 20:26:06 +0000

    Move AWS fields into a proper struct

--- services/config.go
+++ services/config.go
@@ -1,9 +1,12 @@
 package services
 
+// AWSConfig contains credentials for using AWS services.
+type AWSConfig struct {
+	Key    string
+	Secret string
+}
+
 // Config contains any configuration data for the service functions.
 type Config struct {
-	AWS struct {
-		Key    string
-		Secret string
-	}
+	AWS *AWSConfig
 }
