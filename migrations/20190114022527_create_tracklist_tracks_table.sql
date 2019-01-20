4baad61b9f1444536ba21310300875e7be3ffbe8
Tom Bell <tomb@tomb.io>
2019-01-20 02:55:33 +0000

    Add ON DELETE CASCADE to tracklist_id column

--- migrations/20190114022527_create_tracklist_tracks_table.sql
+++ migrations/20190114022527_create_tracklist_tracks_table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE tracklist_tracks (
   id           UUID PRIMARY KEY,
-  tracklist_id UUID REFERENCES tracklists (id),
+  tracklist_id UUID REFERENCES tracklists (id) ON DELETE CASCADE,
   track_id     UUID REFERENCES tracks (id)
 );
 
