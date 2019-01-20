3b3067b4289a2602b935a1ae2a5dad118c6acfba
Tom Bell <tomb@tomb.io>
2019-01-20 14:05:55 +0000

    Rewrite UploadFile using s3manager for uploading

--- services/upload.go
+++ services/upload.go
@@ -1,14 +1,12 @@
 package services
 
 import (
-	"bytes"
-	"net/http"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 const (
@@ -17,39 +15,28 @@ const (
 )
 
 // UploadFile uploads a single file at the given path to S3.
-func (s *Services) UploadFile(filepath string, key string) error {
+func (s *Services) UploadFile(filepath string, key string) (string, error) {
 	f, err := os.Open(filepath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer f.Close()
 
-	info, err := f.Stat()
-	if err != nil {
-		return err
-	}
-
-	buf := make([]byte, info.Size())
-
-	if _, err := f.Read(buf); err != nil {
-		return nil
-	}
-
-	object := &s3.PutObjectInput{
-		Bucket:        aws.String(defaultS3Bucket),
-		Key:           aws.String(key),
-		Body:          bytes.NewReader(buf),
-		ContentLength: aws.Int64(info.Size()),
-		ContentType:   aws.String(http.DetectContentType(buf)),
+	input := &s3manager.UploadInput{
+		Bucket: aws.String(defaultS3Bucket),
+		Key:    aws.String(key),
+		Body:   f,
 	}
 
 	creds := credentials.NewStaticCredentials(s.Config.AWS.Key, s.Config.AWS.Secret, "")
 	cfg := aws.NewConfig().WithCredentials(creds).WithRegion(defaultAWSRegion)
-	client := s3.New(session.New(), cfg)
 
-	if _, err := client.PutObject(object); err != nil {
-		return err
+	uploader := s3manager.NewUploader(session.New(cfg))
+
+	result, err := uploader.Upload(input)
+	if err != nil {
+		return "", err
 	}
 
-	return nil
+	return result.Location, nil
 }
