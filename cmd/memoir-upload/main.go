c2dfed232b91b3efe7fb7233faa5736ed7a45da1
Tom Bell <tomb@tomb.io>
2019-01-20 20:55:50 +0000

    Update memoir-upload with initial implementation

--- cmd/memoir-upload/main.go
+++ cmd/memoir-upload/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"path/filepath"
+
+	"github.com/tombell/memoir/database"
+	"github.com/tombell/memoir/services"
 )
 
 const helpText = `usage: memoir-upload [args] <path to mix mp3>
 
   --db         connection string for connecting to the database
   --tracklist  name of the tracklist to associate the uploaded mix with
+  --aws-key
+  --aws-secret
 
 Special options:
   --help      show this message, then exit
@@ -19,12 +27,28 @@ Special options:
 var (
 	dsn       = flag.String("db", "", "")
 	tracklist = flag.String("tracklist", "", "")
+	awsKey    = flag.String("aws-key", "", "")
+	awsSecret = flag.String("aws-secret", "", "")
 	version   = flag.Bool("version", false, "")
 )
 
 func usage() {
 	fmt.Fprintf(os.Stderr, helpText)
 	os.Exit(2)
+}
+
+func validateFlags() {
+	if *dsn == "" {
+		flag.Usage()
+	}
+
+	if *tracklist == "" {
+		flag.Usage()
+	}
+
+	if *awsKey == "" || *awsSecret == "" {
+		flag.Usage()
+	}
 }
 
 func main() {
@@ -36,7 +60,54 @@ func main() {
 		os.Exit(0)
 	}
 
-	if *dsn == "" {
+	args := flag.Args()
+	if len(args) != 1 {
 		flag.Usage()
 	}
+
+	validateFlags()
+
+	logger := log.New(os.Stderr, "", 0)
+
+	db, err := database.Open(*dsn)
+	if err != nil {
+		logger.Fatalf("err: %v\n", err)
+	}
+
+	f, err := os.Open(args[0])
+	if err != nil {
+		logger.Fatalf("err: %v\n", err)
+	}
+	defer f.Close()
+
+	var buf []byte
+
+	if _, err := f.Read(buf); err != nil {
+		logger.Fatalf("err: %v\n", err)
+	}
+
+	contentType := http.DetectContentType(buf)
+
+	cfg := &services.Config{
+		AWS: &services.AWSConfig{
+			Key:    *awsKey,
+			Secret: *awsSecret,
+		},
+	}
+
+	svc := &services.Services{
+		Config: cfg,
+		Logger: logger,
+		DB:     db,
+	}
+
+	// TODO: slugify for nicer filename?
+	key := filepath.Base(args[0])
+
+	location, err := svc.Upload(f, key, contentType)
+	if err != nil {
+		logger.Fatalf("err: %v\n", err)
+	}
+
+	logger.Printf("file uploaded to %s\n", location)
 }
