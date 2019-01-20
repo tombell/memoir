1813a60ed379780ded62a272c5b6c2aabd8b4ff2
Tom Bell <tomb@tomb.io>
2019-01-20 04:27:32 +0000

    Add create_mix_uploads_table migration

---
+++ migrations/20190120042003_create_mix_uploads_table.sql
@@ -0,0 +1,18 @@
+-- UP
+
+CREATE TABLE mix_uploads (
+  id           UUID PRIMARY KEY,
+  tracklist_id UUID REFERENCES tracklists (id) ON DELETE CASCADE,
+  filename     VARCHAR(256) UNIQUE NOT NULL,
+  key          VARCHAR(256) UNIQUE NOT NULL,
+  created      TIMESTAMP NOT NULL,
+  updated      TIMESTAMP NOT NULL
+);
+
+CREATE INDEX mix_uploads_tracklist_id_idx ON mix_uploads (tracklist_id);
+
+-- DOWN
+
+DROP INDEX IF EXISTS mix_uploads_tracklist_id_idx CASCADE;
+
+DROP TABLE IF EXISTS mix_uploads CASCADE;
