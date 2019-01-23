e08d2619d072d65fe8d26182a882cb6c03036d8d
Tom Bell <tomb@tomb.io>
2019-01-23 00:58:30 +0000

    Add get tracklist with tracks by name query

--- database/queries_tracklists.go
+++ database/queries_tracklists.go
@@ -49,6 +49,23 @@ const (
 		WHERE name = $1
 		LIMIT 1`
 
+	sqlGetTracklistWithTracksByName = `
+		SELECT
+			tl.*,
+			t.id as track_id,
+			t.artist,
+			t.name,
+			t.genre,
+			t.bpm,
+			t.key,
+			t.created,
+			t.updated
+		FROM tracklists tl
+		JOIN tracklist_tracks tt ON tt.tracklist_id = tl.id
+		JOIN tracks t ON t.id = tt.track_id
+		WHERE tl.name = $1
+		ORDER BY tt.track_number ASC`
+
 	sqlRemoveTracklist = `
 		DELETE FROM tracklists
 		WHERE id = $1`
