7e402a0e6c6669c8a0e2da3631c3cb9804aa5499
Tom Bell <tomb@tomb.io>
2019-01-24 04:35:04 +0000

    Add comments to task interface

--- task/task.go
+++ task/task.go
@@ -2,6 +2,9 @@ package task
 
 // Task is the interface that wraps a piece of runnable functionality.
 type Task interface {
+	// Name returns the name of the task.
 	Name() string
+
+	// Run executes the task.
 	Run() error
 }
