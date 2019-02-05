58ae1a801ca913681873d6491aa2d9deae351ce8
Tom Bell <tomb@tomb.io>
2019-02-05 00:05:52 +0000

    Remove unused AWSConfig struct, and field

--- services/config.go
+++ services/config.go
@@ -1,12 +1,4 @@
 package services
 
-// AWSConfig contains credentials for using AWS services.
-type AWSConfig struct {
-	Key    string
-	Secret string
-}
-
 // Config contains any configuration data for the service functions.
-type Config struct {
-	AWS *AWSConfig
-}
+type Config struct{}
