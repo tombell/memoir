74a5ede66a625c22e37356e85310f82de7a0cd61
Tom Bell <tomb@tomb.io>
2019-02-17 01:59:22 +0000

    Add http.Error calls

--- api/handler_tracklists_index.go
+++ api/handler_tracklists_index.go
@@ -9,12 +9,12 @@ func (s *Server) handleTracklistsIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tracklists, err := s.services.RecentTracklists()
 		if err != nil {
-			// TODO
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		if err := json.NewEncoder(w).Encode(tracklists); err != nil {
-			// TODO
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
