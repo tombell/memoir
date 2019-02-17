1ee76ee0c3815c8c28a108c49b81b862a7f0d006
Tom Bell <tomb@tomb.io>
2019-02-17 03:16:47 +0000

    Add requestID middleware

--- api/middleware.go
+++ api/middleware.go
@@ -1,10 +1,32 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+)
+
+type contextKey string
+
+const requestIDKey contextKey = "requestid"
 
 func (s *Server) json(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/json")
+		h(w, r)
+	}
+}
+
+func (s *Server) requestID(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id, _ := uuid.NewV4()
+
+		w.Header().Add("X-Request-ID", id.String())
+
+		ctx := context.WithValue(r.Context(), requestIDKey, id.String())
+		r.WithContext(ctx)
+
 		h(w, r)
 	}
 }
