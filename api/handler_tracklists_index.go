70a01cdec962132b4a0ba9e8c09d0c8e548217a8
Tom Bell <tomb@tomb.io>
2019-02-17 01:55:16 +0000

    Add simple tracklists index handler

---
+++ api/handler_tracklists_index.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (s *Server) handleTracklistsIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tracklists, err := s.services.RecentTracklists()
+		if err != nil {
+			// TODO
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(tracklists); err != nil {
+			// TODO
+			return
+		}
+	}
+}
