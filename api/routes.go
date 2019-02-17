9019c4c828a04a57443df64c62e0aee891c90ef5
Tom Bell <tomb@tomb.io>
2019-02-17 03:17:07 +0000

    Add requestID middleware to routes

--- api/routes.go
+++ api/routes.go
@@ -1,6 +1,6 @@
 package api
 
 func (s *Server) routes() {
-	s.router.Handle("GET", "/tracklists", s.json(s.handleGetTracklists()))
-	s.router.Handle("GET", "/tracklists/:id", s.json(s.handleGetTracklist()))
+	s.router.Handle("GET", "/tracklists", s.requestID(s.json(s.handleGetTracklists())))
+	s.router.Handle("GET", "/tracklists/:id", s.requestID(s.json(s.handleGetTracklist())))
 }
