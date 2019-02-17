2fbcfda5b28146c548fdd5ff922284edec5d4cb5
Tom Bell <tomb@tomb.io>
2019-02-17 00:57:54 +0000

    Add wip server type in api package

---
+++ api/api.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/matryer/way"
+
+	"github.com/tombell/memoir/services"
+)
+
+// Config ...
+type Config struct {
+	Logger   *log.Logger
+	Services *services.Services
+}
+
+// Server ...
+type Server struct {
+	logger *log.Logger
+	router *way.Router
+
+	services *services.Services
+}
+
+// Start ...
+func (s *Server) Start(addr string) error {
+	s.routes()
+
+	return http.ListenAndServe(addr, s.router)
+}
+
+// NewServer ...
+func NewServer(cfg *Config) *Server {
+	return &Server{
+		logger:   cfg.Logger,
+		router:   way.NewRouter(),
+		services: cfg.Services,
+	}
+}
---
+++ api/routes.go
@@ -0,0 +1,4 @@
+package api
+
+func (s *Server) routes() {
+}
