c0417fb7be86dc44419758a25f5418a941f06f21
Tom Bell <tomb@tomb.io>
2019-02-21 21:21:28 +0000

    Add CORS header middleware to routes

--- api/routes.go
+++ api/routes.go
@@ -1,6 +1,6 @@
 package api
 
 func (s *Server) routes() {
-	s.router.Handle("GET", "/tracklists", use(s.handleGetTracklists(), s.json, s.instruments, s.requestID))
-	s.router.Handle("GET", "/tracklists/:id", use(s.handleGetTracklist(), s.json, s.instruments, s.requestID))
+	s.router.Handle("GET", "/tracklists", use(s.handleGetTracklists(), s.cors, s.json, s.instruments, s.requestID))
+	s.router.Handle("GET", "/tracklists/:id", use(s.handleGetTracklist(), s.cors, s.json, s.instruments, s.requestID))
 }
