ca1aaed63096e5966cb658b95d7dfeb5024776d5
Tom Bell <tomb@tomb.io>
2019-02-21 21:21:21 +0000

    Add CORS header middleware

--- api/middleware.go
+++ api/middleware.go
@@ -29,6 +29,16 @@ func (s *Server) json(h http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func (s *Server) cors(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+		w.Header().Add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+
+		h(w, r)
+	}
+}
+
 func (s *Server) requestID(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		id, _ := uuid.NewV4()
