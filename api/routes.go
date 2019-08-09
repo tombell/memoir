839db9cd7e5abe1ea83c0b555b0fe862111535eb
Tom Bell <tomb@tomb.io>
2019-08-09 23:41:45 +0100

    Fix ordering to make sure route is registered

--- api/routes.go
+++ api/routes.go
@@ -4,6 +4,6 @@ func (s *Server) routes() {
 	s.router.Handle("GET", "/tracklists", use(s.handleGetTracklists(), s.cors, s.json, s.instruments, s.requestID))
 	s.router.Handle("GET", "/tracklists/:id", use(s.handleGetTracklist(), s.cors, s.json, s.instruments, s.requestID))
 
-	s.router.Handle("GET", "/tracks/mostplayed", use(s.handleGetMostPlayedTracks(), s.cors, s.json, s.instruments, s.requestID))
 	s.router.Handle("GET", "/tracks/:id/tracklists", use(s.handleGetTracklistsByTrack(), s.cors, s.json, s.instruments, s.requestID))
+	s.router.Handle("GET", "/tracks/mostplayed", use(s.handleGetMostPlayedTracks(), s.cors, s.json, s.instruments, s.requestID))
 }
