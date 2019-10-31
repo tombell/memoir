2d1afda856f757530450765e6ef3c1ce016837fe
Tom Bell <tomb@tomb.io>
2019-10-31 19:39:15 +0000

    Do not use t.*

--- datastore/tracks.go
+++ datastore/tracks.go
@@ -50,7 +50,14 @@ const (
 
 	sqlFindMostPlayedTracks = `
 		SELECT
-			t.*,
+			t.id,
+			t.artist,
+			t.name,
+			t.genre,
+			t.bpm,
+			t.key,
+			t.created,
+			t.updated,
 			count(t.id) as played
 		FROM tracks t
 		JOIN tracklist_tracks tt ON tt.track_id = t.id
