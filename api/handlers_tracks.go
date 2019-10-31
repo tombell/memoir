017900dac10b7c46fcf606e8ea936c6675b8b563
Tom Bell <tomb@tomb.io>
2019-10-31 19:51:55 +0000

    Add handler for searching tracks

--- api/handlers_tracks.go
+++ api/handlers_tracks.go
@@ -66,3 +66,25 @@ func (s *Server) handleGetMostPlayedTracks() http.HandlerFunc {
 		w.Write(resp)
 	}
 }
+
+func (s *Server) handleSearchTracks() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rid := getRequestID(r)
+
+		tracks, err := s.services.SearchTracks(r.URL.Query().Get("q"))
+		if err != nil {
+			s.logger.Printf("rid=%s error=%s\n", rid, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		resp, err := json.Marshal(tracks)
+		if err != nil {
+			s.logger.Printf("rid=%s error=%s\n", rid, err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(resp)
+	}
+}
