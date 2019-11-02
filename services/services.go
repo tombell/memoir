c39cbe31f9de2424bae597d3ca10d030757326e9
Tom Bell <tomb@tomb.io>
2019-11-02 23:34:30 +0000

    Move filestore package into pkg directory

--- services/services.go
+++ services/services.go
@@ -3,9 +3,9 @@ package services
 import (
 	"log"
 
-	"github.com/tombell/memoir/filestore"
 	"github.com/tombell/memoir/pkg/config"
 	"github.com/tombell/memoir/pkg/datastore"
+	"github.com/tombell/memoir/pkg/filestore"
 )
 
 // Services contains shared dependencies required by service methods.
