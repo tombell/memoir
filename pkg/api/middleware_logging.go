ade7f7a9a9d63145be848defddc2f35e4db9d523
Tom Bell <tomb@tomb.io>
2020-05-23 12:46:16 +0100

    Refactor logging middleware

--- pkg/api/middleware_logging.go
+++ pkg/api/middleware_logging.go
@@ -5,17 +5,52 @@ import (
 	"time"
 )
 
-func (s *Server) instruments(h http.HandlerFunc) http.HandlerFunc {
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (rw *responseWriter) WriteHeader(s int) {
+	rw.status = s
+	rw.ResponseWriter.WriteHeader(s)
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	if rw.status == 0 {
+		rw.WriteHeader(http.StatusOK)
+	}
+
+	size, err := rw.ResponseWriter.Write(b)
+	rw.size += size
+	return size, err
+}
+
+func (s *Server) logging(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		rid := getRequestID(r)
 
 		start := time.Now().UTC()
-		method := r.Method
-		path := r.URL.Path
-		addr := r.RemoteAddr
 
-		s.services.Logger.Printf("rid=%s method=%s path=%s ip=%s\n", rid, method, path, addr)
-		h(w, r)
-		s.services.Logger.Printf("rid=%s method=%s path=%s ip=%s elapsed=%s\n", rid, method, path, addr, time.Since(start))
+		s.services.Logger.Printf(
+			"[%s] method=%s path=%s",
+			rid,
+			r.Method,
+			r.URL.Path,
+		)
+
+		rw := &responseWriter{ResponseWriter: w}
+
+		h(rw, r)
+
+		s.services.Logger.Printf(
+			"[%s] method=%s path=%s status=%d size=%d elapsed=%s\n",
+			rid,
+			r.Method,
+			r.URL.Path,
+			rw.status,
+			rw.size,
+			time.Since(start),
+		)
 	}
 }
--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -2,36 +2,36 @@ package api
 
 func (s *Server) routes() {
 	s.router.Handle("OPTIONS", "/...", use(s.handlePreflight(), s.cors))
-	s.router.Handle("GET", "/healthz", use(s.handleHealth(), s.instruments, s.requestID))
+	s.router.Handle("GET", "/healthz", use(s.handleHealth(), s.logging, s.requestID))
 
 	// Tracklists
 
 	s.router.Handle("GET", "/tracklists",
-		use(s.handleGetTracklists(), s.json, s.cors, s.instruments, s.requestID))
+		use(s.handleGetTracklists(), s.json, s.cors, s.logging, s.requestID))
 
 	s.router.Handle("POST", "/tracklists",
-		use(s.handleImportTracklist(), s.json, s.cors, s.auth, s.instruments, s.requestID))
+		use(s.handleImportTracklist(), s.json, s.cors, s.auth, s.logging, s.requestID))
 
 	s.router.Handle("GET", "/tracklists/:id",
-		use(s.handleGetTracklist(), s.json, s.cors, s.instruments, s.requestID))
+		use(s.handleGetTracklist(), s.json, s.cors, s.logging, s.requestID))
 
 	s.router.Handle("PATCH", "/tracklists/:id",
-		use(s.handleUpdateTracklist(), s.json, s.cors, s.auth, s.instruments, s.requestID))
+		use(s.handleUpdateTracklist(), s.json, s.cors, s.auth, s.logging, s.requestID))
 
 	// Tracks
 
 	s.router.Handle("GET", "/tracks/:id/tracklists",
-		use(s.handleGetTracklistsByTrackID(), s.json, s.cors, s.instruments, s.requestID))
+		use(s.handleGetTracklistsByTrackID(), s.json, s.cors, s.logging, s.requestID))
 
 	s.router.Handle("GET", "/tracks/mostplayed",
-		use(s.handleGetMostPlayedTracks(), s.json, s.cors, s.instruments, s.requestID))
+		use(s.handleGetMostPlayedTracks(), s.json, s.cors, s.logging, s.requestID))
 
 	s.router.Handle("GET", "/tracks/search",
-		use(s.handleSearchTracks(), s.json, s.cors, s.instruments, s.requestID))
+		use(s.handleSearchTracks(), s.json, s.cors, s.logging, s.requestID))
 
 	// Uploads
 
 	s.router.Handle("POST", "/uploads/artwork",
-		use(s.handleUploadArtwork(), s.json, s.cors, s.auth, s.instruments, s.requestID))
+		use(s.handleUploadArtwork(), s.json, s.cors, s.auth, s.logging, s.requestID))
 
 }
