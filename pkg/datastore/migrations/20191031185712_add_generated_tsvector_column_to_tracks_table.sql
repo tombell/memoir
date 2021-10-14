7f1332d4bdf2a15f8e8cf80ad2da58dc45eb65f4
Tom Bell <tomb@tomb.io>
2021-10-14 17:50:27 +0100

    Refactor the tsvector column migration to make a generated column

---
+++ pkg/datastore/migrations/20191031185712_add_generated_tsvector_column_to_tracks_table.sql
@@ -0,0 +1,17 @@
+-- UP
+
+ALTER TABLE tracks
+ADD COLUMN fts_name_and_artist TSVECTOR
+GENERATED ALWAYS AS (
+  setweight(to_tsvector('english', coalesce(name, '')), 'A')
+  || ' ' ||
+  setweight(to_tsvector('english', coalesce(artist, '')), 'B')
+) STORED;
+
+CREATE INDEX tracks_fts_name_and_artist_idx ON tracks USING GIN(fts_name_and_artist);
+
+-- DOWN
+
+DROP INDEX IF EXISTS tracks_fts_name_and_artist_idx;
+
+ALTER TABLE tracks DROP fts_name_and_artist;
