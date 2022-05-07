576ac74efc471d13b73b53dd9adf1fe40a626fd4
Tom Bell <tomb@tomb.io>
2022-05-07 02:34:56 +0100

    wip: format

--- pkg/datastore/queries/tracklists_tracks.sql
+++ pkg/datastore/queries/tracklists_tracks.sql
@@ -1,7 +1,4 @@
 -- name: AddTracklistTrack :exec
-INSERT INTO tracklist_tracks (
-  id,
-  tracklist_id,
-  track_id,
-  track_number
-) VALUES ($1, $2, $3, $4);
+INSERT INTO tracklist_tracks (id, tracklist_id, track_id, track_number)
+    VALUES ($1, $2, $3, $4);
+
--- pkg/datastore/queries/tracks.sql
+++ pkg/datastore/queries/tracks.sql
@@ -1,59 +1,58 @@
 -- name: AddTrack :exec
-INSERT INTO tracks (
-  id,
-  artist,
-  name,
-  genre,
-  bpm,
-  key,
-  created,
-  updated
-) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
+INSERT INTO tracks (id, artist, name, genre, bpm, key, created, updated)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
 
 -- name: FindTrackByID :one
 SELECT
-  id,
-  artist,
-  name,
-  genre,
-  bpm,
-  key,
-  created,
-  updated
-FROM tracks
-WHERE id = $1
+    id,
+    artist,
+    name,
+    genre,
+    bpm,
+    key,
+    created,
+    updated
+FROM
+    tracks
+WHERE
+    id = $1
 LIMIT 1;
 
 -- name: FindTrackByArtistAndName :many
 SELECT
-  id,
-  artist,
-  name,
-  genre,
-  bpm,
-  key,
-  created,
-  updated
-FROM tracks
-WHERE artist = $1
-AND name = $2
+    id,
+    artist,
+    name,
+    genre,
+    bpm,
+    key,
+    created,
+    updated
+FROM
+    tracks
+WHERE
+    artist = $1
+    AND name = $2
 LIMIT 1;
 
 -- name: FindMostPlayedTracks :many
 SELECT
-  t.id,
-  t.artist,
-  t.name,
-  t.genre,
-  t.bpm,
-  t.key,
-  t.created,
-  t.updated,
-  count(t.id) as played
-FROM tracks t
-JOIN tracklist_tracks tt ON tt.track_id = t.id
-GROUP BY t.id
-ORDER BY played DESC
+    t.id,
+    t.artist,
+    t.name,
+    t.genre,
+    t.bpm,
+    t.key,
+    t.created,
+    t.updated,
+    count(t.id) AS played
+FROM
+    tracks t
+    JOIN tracklist_tracks tt ON tt.track_id = t.id
+GROUP BY
+    t.id
+ORDER BY
+    played DESC
 LIMIT $1;
 
 -- TODO: make work with sqlc
