781d4ca76642cbb481cc447f4550aa55ad99b4a3
Tom Bell <tomb@tomb.io>
2023-02-21 20:26:45 +0000

    Add middleware package

---
+++ internal/api/middleware/logging.go
@@ -0,0 +1,58 @@
+package middleware
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/charmbracelet/log"
+)
+
+type responseWriter struct {
+	http.ResponseWriter
+	status int
+	size   int
+}
+
+func (rw *responseWriter) WriteHeader(s int) {
+	rw.status = s
+}
+
+func (rw *responseWriter) Write(b []byte) (int, error) {
+	size, err := rw.ResponseWriter.Write(b)
+	rw.size += size
+	return size, err
+}
+
+func Logging(logger log.Logger) Middleware {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now().UTC()
+
+			logger.Info(
+				"http",
+				"method",
+				r.Method,
+				"path",
+				r.URL.Path,
+			)
+
+			rw := &responseWriter{ResponseWriter: w}
+
+			h(rw, r)
+
+			logger.Info(
+				"http",
+				"method",
+				r.Method,
+				"path",
+				r.URL.Path,
+				"status",
+				rw.status,
+				"size",
+				rw.size,
+				"elapsed",
+				time.Since(start),
+			)
+		}
+	}
+}
