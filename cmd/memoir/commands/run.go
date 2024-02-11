96f2f6dc1de2fcb4b9fd914c0395c751ce87c20e
Tom Bell <tomb@tomb.io>
2024-02-11 01:35:41 +0000

    Add syscall.SIGTERM to signal.Notify

--- cmd/memoir/commands/run.go
+++ cmd/memoir/commands/run.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -59,7 +60,7 @@ func Run(logger *slog.Logger) {
 	}()
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
