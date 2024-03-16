7221ff544b1a06c4e5c79dce4eabcc53f77d5560
Tom Bell <tomb@tomb.io>
2024-03-16 16:16:30 +0000

    Add track service Search function

---
+++ internal/services/trackservice/search.go
@@ -0,0 +1,29 @@
+package trackservice
+
+import (
+	"context"
+
+	"github.com/tombell/memoir/internal/services"
+	"github.com/tombell/memoir/internal/stores/trackstore"
+)
+
+const maxSearchResults = 10
+
+type SearchRequest struct {
+	Query string `query:"q"`
+}
+
+type SearchResponse struct {
+	Tracks []*trackstore.Track `json:"tracks"`
+}
+
+func Search(trackStore *trackstore.Store) services.ServiceFunc[SearchRequest, *SearchResponse] {
+	return func(ctx context.Context, input SearchRequest) (*SearchResponse, error) {
+		tracks, err := trackStore.SearchTracks(ctx, input.Query, maxSearchResults)
+		if err != nil {
+			return nil, err
+		}
+
+		return &SearchResponse{Tracks: tracks}, nil
+	}
+}
