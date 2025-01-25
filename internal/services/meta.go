ba643453685b5a87ef517f46355554bae39f158d
Tom Bell <tomb@tombell.dev>
2025-01-25 14:34:12 +0000

    Move Meta struct into its own file

---
+++ internal/services/meta.go
@@ -0,0 +1,17 @@
+package services
+
+import (
+	"math"
+)
+
+type Meta struct {
+	CurrentPage int   `json:"current_page"`
+	TotalPages  int64 `json:"total_pages"`
+}
+
+func NewMeta(current int, total int64) Meta {
+	return Meta{
+		CurrentPage: current,
+		TotalPages:  int64(math.Ceil(float64(total) / float64(10))),
+	}
+}
--- internal/services/services.go
+++ internal/services/services.go
@@ -2,21 +2,8 @@ package services
 
 import (
 	"context"
-	"math"
 )
 
 type ServiceFunc[In, Out any] func(context.Context, In) (Out, error)
 
 type WriteOnlyServiceFunc[Out any] func(context.Context) (Out, error)
-
-type Meta struct {
-	CurrentPage int   `json:"current_page"`
-	TotalPages  int64 `json:"total_pages"`
-}
-
-func NewMeta(current int, total int64) Meta {
-	return Meta{
-		CurrentPage: current,
-		TotalPages:  int64(math.Ceil(float64(total) / float64(10))),
-	}
-}
