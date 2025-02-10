94a9a84c95339f190ceec8ff47a363d7a4cda03a
Tom Bell <tomb@tombell.dev>
2025-02-10 22:24:24 +0000

    Add documentation comments to datastore package

--- internal/stores/datastore/store.go
+++ internal/stores/datastore/store.go
@@ -6,11 +6,15 @@ import (
 	db "github.com/tombell/memoir/internal/database"
 )
 
+// Store is a store used for interacting with the database. Query functions are
+// generated from SQL Queries by SQLC in the database package.
 type Store struct {
 	*pgxpool.Pool
 	*db.Queries
 }
 
+// New returns a new Store that is configured to use the given pool of database
+// connections.
 func New(dbpool *pgxpool.Pool) *Store {
 	return &Store{
 		Pool:    dbpool,
